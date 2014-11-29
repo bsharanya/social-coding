578c31abf58f02664b6d7ab2120a4fe85cdb3994
Madhushree Sreenivasa <s.madhushree@gmail.com>
2014-11-29 09:10:20 -0600

    profile info json

--- app/org_profile.py
+++ app/org_profile.py
@@ -1,50 +1,39 @@
 import sys
 import subprocess
-import simplejson as json
+import flask
 
-#organisation = sys.argv[1]
-
-def get_json_output(request):
-    #Fetch repos url to check the language for each repository
-    process = subprocess.Popen(["curl", "-H", "POST",  request],stdout=subprocess.PIPE)
-    (out, err) = process.communicate()
-
-    json_output_repos = json.loads(out)
-    return json_output_repos
+def main_func(name):
+    file_ptr=open("profile.json","r")
+    json_output = flask.json.load(file_ptr)
 
 
-organisation=sys.argv[1]
+    name=json_output['name']
+    created_at=json_output['created_at']
+    html_url=json_output['html_url']
+    image = json_output['avatar_url']
+    followers=json_output['followers']
+    no_of_public_repos = json_output['public_repos']
 
-request="https://api.github.com/orgs/" + organisation
-
-json_output=get_json_output(request)
-
-name=json_output['name']
-created_at=json_output['created_at']
-html_url=json_output['html_url']
-image = json_output['avatar_url']
-followers=json_output['followers']
-no_of_public_repos = json_output['public_repos']
-
-#Fetch all the repositories of an organisation
-repos_url=json_output['repos_url']
-json_output_repos = get_json_output(repos_url)
-
-#Add  all the languages to set: no duplication
-languages_set = set([])
-for i in range(0,len(json_output_repos)):
-    languages=json_output_repos[i]['language']
-    languages_set.add(languages)
-
-#Create json for overview left pane
-org_json = {"overview" : { "name": name, "created_at": created_at, "html_url": html_url, "avatar_url": image, "languages": []}}
-
-for item in languages_set:
-     org_json["overview"]["languages"].append(item)
-
-file_ptr = open(name+".json", "w")
-json.dump(org_json, file_ptr)
-file_ptr.close()
+    #Fetch all the repositories of an organisation
+    file_repos_ptr=open("repos.json","r")
+    json_output_repos=flask.json.load(file_repos_ptr)
 
 
+    #Add  all the languages to set: no duplication
+    languages_set = set([])
+    for i in range(0,len(json_output_repos)):
+        languages=json_output_repos[i]['language']
+        languages_set.add(languages)
 
+    #Create json for overview left pane
+    org_json = {"profile" : { "name": name, "created_at": created_at, "html_url": html_url, "avatar_url": image, "languages": []}}
+
+    for item in languages_set:
+         org_json["profile"]["languages"].append(item)
+
+    print org_json
+    file_ptr.close()
+    file_repos_ptr.close()
+
+
+#main_func("twi")
