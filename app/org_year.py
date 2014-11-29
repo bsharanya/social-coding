69a2ecdcb2e99fec2056e412801c5459a675820d
Madhushree Sreenivasa <s.madhushree@gmail.com>
2014-11-29 08:23:00 -0600

    new json for year

--- app/org_year.py
+++ app/org_year.py
@@ -1,7 +1,9 @@
 __author__ = 'madhushrees'
 
+
 import subprocess
 import simplejson as json
+import flask
 
 
 sum_followers=0
@@ -41,62 +43,51 @@ def get_normalized_followers(json_output_repos,sum_followers):
         sum_followers+=followers
 
     for i in range(0,len(all_repos_followers)):
-        normalize_follow = float(all_repos_followers[i]/sum_followers)*10000
+        normalize_follow = int(float(all_repos_followers[i])/sum_followers*10000)
         normalize_follow_array.append(normalize_follow)
 
     return normalize_follow_array
 
 
-def main_func(org_name,year):
-    organisation=org_name
+def main_func(year):
     year=str(year)
-
-    request="https://SocialCodingCS467:socialcoding123@api.github.com/orgs/" + organisation
-    json_output=get_json_output(request)
-
-    #Fetch the repository url in an organisation
-    repos_url=json_output['repos_url']
-    repos_url=repos_url.replace("https://","https://SocialCodingCS467:socialcoding123@")
-    json_output_repos = get_json_repo_output(repos_url)
-
+    file_ptr=open("repos.json","r")
+    json_output_repos = flask.json.load(file_ptr)
 
     #Json format
-    langugae_json={"repositories" : []}
+    langugae_json={"year" : year, "repositories" : []}
     normalize_follow_array = get_normalized_followers(json_output_repos,0)
     cnt=0
 
     #For each repository, fetch the language details, lines of code and number of followers
     for i in range(0,len(json_output_repos)):
-        languages=json_output_repos[i]['language']
         full_name=json_output_repos[i]['full_name']
         repo_created_at = json_output_repos[i]['created_at'].split('-')[0]
 
         if(repo_created_at==year):
-            language_url = "https://SocialCodingCS467:socialcoding123@api.github.com/repos/"+full_name+"/"+"languages"
-            json_output_languages_list = get_json_output(language_url)
+            json_output_languages_dict = json_output_repos[i]['languages']
+            #print(json_output_languages_dict)
             l =[]
             set_l={}
             sum_languages=0
-            for k,v in json_output_languages_list.items():
+            for k,v in json_output_languages_dict.items():
                 x={"name":str(k),"lines":str(v)}
                 set_l[k]=1
                 sum_languages+=int(v)
 
-            for k,v in json_output_languages_list.items():
-                 x={"name":str(k),"lines":int(v/sum_languages*250)}
+            for k,v in json_output_languages_dict.items():
+                 x={"name":str(k),"lines":int(float(v)/sum_languages*250)}
                  l.append(x)
+            print(l)
 
             langugae_json["repositories"].append({"name":cnt+1, "languages":l,"followers":int(normalize_follow_array[i])})
             cnt+=1
 
-
-    # file_ptr2 = open("samples/"+organisation+"-languages.json","w")
-    # json.dump(langugae_json,file_ptr2)
-    # file_ptr2.close()
-    print(langugae_json)
+    #print(langugae_json)
+    file_ptr.close()
     return langugae_json
 
-# main_func("twitter",2010)
+#main_func(2010)
 
 
 
