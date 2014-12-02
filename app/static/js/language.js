0996ce2e117fbe6750c39e044c3e881ae09c5108
Madhushree Sreenivasa <s.madhushree@gmail.com>
2014-12-02 14:27:36 -0600

    language page changes to accomodate more repos

--- app/static/js/language.js
+++ app/static/js/language.js
@@ -7,16 +7,17 @@ var data = d3.json("/api/language/details", function(error, data) {
     svgContainer.attr("width", '672px')
         .attr("height", '504px');
 
-    var inter_width = 89
+    var inter_width = 85
 
     var root = data.years;
     var keys = Object.keys(data.years)
 
     var language = data.language;
+    var max_value=data.max;
 
 
     var languageText = svgContainer.append("text")
-        .attr("x", 60)
+        .attr("x", 80)
         .attr("y", 50)
         .text(function(){
             return language;
@@ -26,11 +27,23 @@ var data = d3.json("/api/language/details", function(error, data) {
         .attr("text-anchor", "left")
         .attr("font-family", "PT Sans");
 
+    var legend1 = svgContainer.append("text")
+        .attr("x", 0)
+        .attr("y", 80)
+        .text(function(){
+            return  "Percentage";
+        })
+        .attr("fill", "gray")
+        .attr("font-size", "13")
+        .attr("text-anchor", "left")
+        .attr("font-family", "PT Sans");
+
+
 
     var line0 = svgContainer.append("line")
         .attr("stroke-width", 2)
         .attr("stroke", "gray")
-        .attr("x1", 60)
+        .attr("x1", 80)
         .attr("y1", 60)
         .attr("x2", 670)
         .attr("y2", 60);
@@ -38,89 +51,163 @@ var data = d3.json("/api/language/details", function(error, data) {
     var line1 = svgContainer.append("line")
         .attr("stroke-width", 2)
         .attr("stroke", "gray")
-        .attr("x1", 60)
+        .attr("x1", 80)
         .attr("y1", 90)
         .attr("x2", 670)
         .attr("y2", 90);
 
+    var time_legend = svgContainer.append("text")
+        .attr("x", 25)
+        .attr("y", 110)
+        .text(function(){
+            return "Time";
+        })
+        .attr("fill", "gray")
+        .attr("font-size", "13")
+        .attr("text-anchor", "left")
+        .attr("font-family", "PT Sans");
+
     var line2 = svgContainer.append("line")
         .attr("stroke-width", 2)
         .attr("stroke", "gray")
-        .attr("x1", 60)
+        .attr("x1", 80)
         .attr("y1", 120)
         .attr("x2", 670)
         .attr("y2", 120);
 
+    var repository_legend = svgContainer.append("text")
+        .attr("x", 0)
+        .attr("y", 140)
+        .text(function(){
+            return "Repository";
+        })
+        .attr("fill", "gray")
+        .attr("font-size", "13")
+        .attr("text-anchor", "left")
+        .attr("font-family", "PT Sans");
+
 
     for (var i=1;i<8;i++) {
-        for (j = 0; j < data.years[keys[i-1]].repos.length; j++) {
+        for (j = 0; j < data.years[keys[i - 1]].repos.length; j++) {
 
             // First rectangle
-            if(data.years[keys[i-1]].repos.length!=0) {
-                var rectangle = svgContainer.append("rect")
-                    .style("fill", "#E0E0E0")
-                    .attr("x", 71 + (i - 1) * 88)
-                    .attr("y", 130 + (j) * 40)
-                    .transition()
-                    .duration(function () {
-                        return (j) * 700;
-                    })
-                    .delay(function () {
-                        return (j) * 200;
-                    })
-                    .attr("width", 70)
-                    .attr("height", 30);
+            if (data.years[keys[i - 1]].repos.length != 0) {
+                if (max_value < 10) {
+                    var rectangle = svgContainer.append("rect")
+                        .style("fill", "#E0E0E0")
+                        .attr("x", 91 + (i - 1) * 84)
+                        .attr("y", 130 + (j) * 40)
+                        .transition()
+                        .duration(function () {
+                            return (j) * 700;
+                        })
+                        .delay(function () {
+                            return (j) * 200;
+                        })
+                        .attr("width", 70)
+                        //.attr("height", 30);
+                        .attr("height", 30)
 
-                // Text field for project name
-                svgContainer.append("text")
+                    // Text field for project name
+                    svgContainer.append("text")
 
-                    .attr("x", function (d) {
-                        return 80 + (i - 1) * 88;
-                    })
-                    .attr("y", 143 + (j) * 40)
-                    .attr("dy", ".35em")
-                    .transition()
-                    .duration(function () {
-                        return (j+1) * 500;
-                    })
-                    .delay(function () {
-                        return (j+1) * 150;
-                    })
-                    .text(function () {
-                        if (data.years[keys[i-1]].repos.length != 0) {
-                            return data.years[keys[i-1]].repos[j].name.substring(0,7).concat("...");
-                        } else {
-                            return "";
-                        }
-                    })
-                    .attr("fill", "black")
-                    .attr("font-size", "13")
-                    .attr("text-anchor", "left")
-                    .attr("font-family", "PT Sans");
+                        .attr("x", function (d) {
+                            return 100 + (i - 1) * 84;
+                        })
+                        .attr("y", 143 + (j) * 40)
+                        .attr("dy", ".35em")
+                        .transition()
+                        .duration(function () {
+                            return (j + 1) * 500;
+                        })
+                        .delay(function () {
+                            return (j + 1) * 150;
+                        })
+                        .text(function () {
+                            if (data.years[keys[i - 1]].repos.length != 0) {
+                                return data.years[keys[i - 1]].repos[j].name.substring(0, 7).concat("...");
+                            } else {
+                                return "";
+                            }
+                        })
+                        .attr("fill", "black")
+                        .attr("font-size", "13")
+                        .attr("text-anchor", "left")
+                        .attr("font-family", "PT Sans");
+                }
+                else {
+
+                    var rectangle = svgContainer.append("rect")
+                        .style("fill", "#E0E0E0")
+                        .attr("x", 91 + (i - 1) * 84)
+                        .attr("y", 130 + (j) * 20)
+                        .transition()
+                        .duration(function () {
+                            return (j) * 700;
+                        })
+                        .delay(function () {
+                            return (j) * 200;
+                        })
+                        .attr("width", 70)
+                        //.attr("height", 30);
+                        .attr("height", 200 / max_value)
+
+                    // Text field for project name
+                    svgContainer.append("text")
+
+                        .attr("x", function (d) {
+                            return 103 + (i - 1) * 84;
+                        })
+                        .attr("y", 135 + (j) * 20)
+                        .attr("dy", ".35em")
+                        .transition()
+                        .duration(function () {
+                            return (j + 1) * 500;
+                        })
+                        .delay(function () {
+                            return (j + 1) * 150;
+                        })
+                        .text(function () {
+                            if (data.years[keys[i - 1]].repos.length != 0) {
+                                return data.years[keys[i - 1]].repos[j].name.substring(0, 7).concat("...");
+                            } else {
+                                return "";
+                            }
+                        })
+                        .attr("fill", "black")
+                        .attr("font-size", "10")
+                        .attr("text-anchor", "left")
+                        .attr("font-family", "PT Sans");
+
+
+                }
             }
+
+
 
         }
 
-            //Dashed line
-            svgContainer.append("line")
-                .style("stroke-dasharray", ("8, 3"))
-                .attr("stroke-width", 2)
-                .attr("stroke", "gray")
-                .attr("opacity", 0.5)
-                .attr("x1", function () {
-                    return 60 + (i) * inter_width;
-                })
-                .attr("y1", 60)
-                .attr("x2", function () {
-                    return 60 + (i) * inter_width;
-                })
-                .attr("y2", 400);
+        //Dashed line
+        svgContainer.append("line")
+            .style("stroke-dasharray", ("8, 3"))
+            .attr("stroke-width", 2)
+            .attr("stroke", "gray")
+            .attr("opacity", 0.5)
+            .attr("x1", function () {
+                return 80 + (i) * inter_width;
+            })
+            .attr("y1", 60)
+            .attr("x2", function () {
+                return 80 + (i) * inter_width;
+            })
+            //.attr("y2", 400);
+            .attr("y2", 500);
 
 
-            //Year sbg element
+        //Year sbg element
             svgContainer.append("text")
                 .attr("x", function () {
-                    return 83 + (i - 1) * inter_width;
+                    return 103 + (i - 1) * inter_width;
                 })
                 .attr("y", 110)
                 .text(function () {
@@ -132,59 +219,60 @@ var data = d3.json("/api/language/details", function(error, data) {
                 .attr("font-family", "PT Sans");
 
 
-            //Total bar for repositories
-            svgContainer.append("line")
-                .attr("stroke-width", 10)
-                .attr("stroke", "#E0E0E0")
-                .attr("y1", function () {
-                    return 75;
-                })
-                .attr("x1", function () {
-                    return 67 + (i - 1) * inter_width;
-                })
-                .attr("y2", function () {
-                    return 75;
-                })
-                .attr("x2", function () {
-                    return 133 + (i - 1) * inter_width;
-                });
 
-            // No of repositories
+        //Total bar for repositories
+        svgContainer.append("line")
+            .attr("stroke-width", 10)
+            .attr("stroke", "#E0E0E0")
+            .attr("y1", function () {
+                return 75;
+            })
+            .attr("x1", function () {
+                return 87 + (i - 1) * inter_width;
+            })
+            .attr("y2", function () {
+                return 75;
+            })
+            .attr("x2", function () {
+                return 153 + (i - 1) * inter_width;
+            });
 
-            svgContainer.append("line")
-                .attr("stroke-width", 10)
-                .attr("stroke", "blue")
-                .attr("y1", function () {
-                    return 75;
-                })
-                //.attr("x1", 363)
-                .attr("x1", function () {
-                    return 67 + (i - 1) * inter_width;
-                })
-                .attr("y2", function () {
-                    return 75;
-                })
-                //.attr("x1", 363)
-                .attr("x2", function () {
-                    return 67 + (i - 1) * inter_width;
-                })
-                .transition()
-                .duration(function () {
-                    return (i) * 300;
-                })
-                .delay(function () {
-                    return (i) * 100;
-                })
-                .attr("y2", function () {
-                    return 75;
-                })
-                .attr("x2", function () {
-                    if(data.years[keys[i-1]].repos.length!=0) {
-                        return 67 + data.years[keys[i - 1]].ratio + (i - 1) * inter_width;
-                    }else{
-                        return 67+(i-1)*inter_width;
-                    }
-                });
-        }
+        // No of repositories
+
+        svgContainer.append("line")
+            .attr("stroke-width", 10)
+            .attr("stroke", "#3d67ea")
+            .attr("y1", function () {
+                return 75;
+            })
+            //.attr("x1", 363)
+            .attr("x1", function () {
+                return 87 + (i - 1) * inter_width;
+            })
+            .attr("y2", function () {
+                return 75;
+            })
+            //.attr("x1", 363)
+            .attr("x2", function () {
+                return 87 + (i - 1) * inter_width;
+            })
+            .transition()
+            .duration(function () {
+                return (i) * 300;
+            })
+            .delay(function () {
+                return (i) * 100;
+            })
+            .attr("y2", function () {
+                return 75;
+            })
+            .attr("x2", function () {
+                if (data.years[keys[i - 1]].repos.length != 0) {
+                    return 87 + data.years[keys[i - 1]].ratio + (i - 1) * inter_width;
+                } else {
+                    return 87 + (i - 1) * inter_width;
+                }
+            });
+    }
 
 })
